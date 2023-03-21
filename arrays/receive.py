0412b103663aada0b4ef4d686a9b854977bc62b1
KiranTheRam <kramji12@gmail.com>
2023-03-21 16:54:36 -0400

    Update receive.py

--- arrays/receive.py
+++ arrays/receive.py
@@ -8,4 +8,6 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
 sock.bind((UDP_IP, UDP_PORT))
 while True:
     data, addr = sock.recvfrom(1024)
+
+    # Deserialize (and print) the data
     print("received message:", pickle.loads(data))
