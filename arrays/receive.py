78ff21bff11b1544af9ee09b835e4e879514faa6
KiranTheRam <kramji12@gmail.com>
2023-03-28 20:45:59 -0400

    Update receive.py

--- arrays/receive.py
+++ arrays/receive.py
@@ -1,5 +1,9 @@
+import json
 import socket
+import requests
 import pickle
+
+api_url = "https://bgpj1c.deta.dev/postroom"
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5001
@@ -9,5 +13,12 @@ sock.bind((UDP_IP, UDP_PORT))
 while True:
     data, addr = sock.recvfrom(1024)
 
-    # Deserialize (and print) the data
-    print("received message:", pickle.loads(data))
+    # Deserialize the data
+    deserialized_data = pickle.loads(data)
+    print("received message:", deserialized_data)
+
+    room_info = '{"roomID": ' + deserialized_data[0] + ', "distance": ' + deserialized_data[2] + ', "deviceID":' + deserialized_data[1] + '}'
+    response = requests.post(api_url, json.loads(room_info))
+
+    print(response.json())
+    print("\n\n")
